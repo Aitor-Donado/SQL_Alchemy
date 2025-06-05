88fab6eda03c1de27cc4e9959f211baf002ad0f0
Aitor Donado <aitor.donado@gmail.com>
2025-06-05 12:53:32 +0200

    Gestor de datos de reseñas como clase

--- biblioteca/gestor_resenias.py
+++ biblioteca/gestor_resenias.py
@@ -1,52 +1,29 @@
-from pymongo import MongoClient
-
 import environ
 env = environ.Env()
 env.read_env(".env")
 # Cadena de conexión de MongoDB Atlas
 connection_string = env("connection_string")
 
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from typing import Dict
 
-# Conectar a la base de datos
-client = MongoClient(connection_string)
+class GestorDatosResenias:
+    def __init__(self, uri="mongodb://localhost:27017", db_name="biblioteca"):
+        self.client = MongoClient(uri)
+        self.db = self.client[db_name]
+        self.coleccion = self.db["resenias"]
 
-# Verificar la conexión
-try:
-    # Listar las bases de datos disponibles
-    databases = client.list_database_names()
-    print("Conexión exitosa a MongoDB")
-    print("Bases de datos disponibles:", databases)
-except Exception as e:
-    print("Error al conectar a MongoDB:", e)
-
-
-db = client['biblioteca']
-collection = db['resenias']
-
-nueva_resenia = {
-    "autor": "55E58A",
-    "material": "ABC123",
-    "resenia": "Muy buen libro. Me ha gustado",
-    "mencionar_usuarios": ["B6EDE9"],
-    "calificacion": 5,
-    "fecha": ""
-}   
-
-collection.insert_one(nueva_resenia)
-
-def listar_resenias():
-    return list(collection.find({"autor": "55E58A", "material": "ABC123"}))
-
-lista_resenias = listar_resenias()
-print(lista_resenias)
-
-# Todos los documentos
-for usuario in db.resenias.find():
-    print(usuario)
-
-
-def borrar_resenia(autor, material):
-    collection.delete_one({"autor": autor, "material": material})
-    return "Resenia borrada correctamente"
-borrar = borrar_resenia("55E58A", "ABC123")
-print(borrar)  # Debería imprimir "Resenia borrada correctamente" si se borró correctamente,
+    def aniadir_resenia(self, resenia: Dict) -> str:
+        resultado = self.coleccion.insert_one(resenia)
+        return str(resultado.inserted_id)
+    
+    def listar_resenias_todas(self) -> list:
+        return list(self.coleccion.find())
+    
+    def listar_resenias_usu(self, id_material: str) -> list:
+        return list(self.coleccion.find({"id_usuario": id_material}))
+    
+    def listar_resenias_mat(self, id_material: str) -> list:
+        return list(self.coleccion.find({"id_material": id_material}))
+    
